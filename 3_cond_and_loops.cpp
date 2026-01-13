a1778adb5c7568d4b61f0c5f5d1e0f92d7239af1
Sneha <snehasaini143786@gmail.com>
2026-01-12 23:15:09 -0200

    Files added

---
+++ 3_cond_and_loops.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{ //1
+   // int n;
+   // cin>>n;
+    //cout<<"Value of n is: "<<n<<endl;
+
+    //2
+    /*
+    int a;
+    cout<<"Enter a: ";
+    cin>>a;
+    if(a>0)
+    cout<<"A is positive"<<endl;
+    else 
+    cout<<"A is negative"<<endl;
+    */
+   //3
+   int a,b;
+   cin>>a>>b;
+   cout<<"Value"
+
+
+}
