0e1a5bca8567a7d2383b36153f1290cb4767ab9a
Sneha <snehasaini143786@gmail.com>
2026-04-05 18:03:09 +0530

    commit chnages

---
+++ Lectures_codes.cpp/lecture_5.cpp/variables_scope17.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=3;
+    cout<<a<<endl;
+
+    if(true){
+        int b=5;
+        cout<<b<<endl;
+    }
+
+    int b=1;
+    cout<<b<<endl;
+
+    //int b=3;
+
+    cout << b << endl;
+    int i =8;
+    for(;i<8;i++){
+        cout << " HI " << endl;
+    }
+
+    if(1){
+        int b;
+        if(1){
+            int b;
+            if(1){
+                int b;
+            }
+        }
+    }
+}
