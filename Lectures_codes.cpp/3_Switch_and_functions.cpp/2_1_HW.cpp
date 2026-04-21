fcd98164c388878d550cdc78deef714556523f7a
Sneha <snehasaini143786@gmail.com>
2026-04-21 10:23:35 +0530

    commit chnages

--- Lectures_codes.cpp/3_Switch_and_functions.cpp/2_1_HW.cpp
+++ Lectures_codes.cpp/3_Switch_and_functions.cpp/2_1_HW.cpp
@@ -2,5 +2,35 @@
 using namespace std;
 int main()
 {
-    
+    int amount , hun , fifty , twenty , one ;
+    cin >> amount;
+
+    int rupee;
+    cin >> rupee;
+    cout << rupee;
+
+    int rupee;
+    cout << "Enter the rupee note which you want to find : " <<endl;
+    cin >> rupee;
+
+    switch ( rupee ) {
+        case 100 :
+            hun = amount / 100; 
+            cout << " 100 rupee notes are : " << hun;
+        
+        case 50 : 
+            hun = amount / 100;
+            int a = amount - ( hun*100 ); 
+            fifty = a / 50;
+          
+        case 20 :
+            
+
+                
+
+                
+
+    }
+
+
 }
---
+++ Lectures_codes.cpp/4_Array.cpp/1_arrayIntro.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    //declare
+    int number[15];
+    
+    //accessing an arry
+    cout << "Value at 15 index " << number[6] <<endl;
+
+    cout << "Value at 20th index  " << number[30] << endl;
+           
+    cout << endl << "Everything is fine" << endl;
+    return 0;
+}
