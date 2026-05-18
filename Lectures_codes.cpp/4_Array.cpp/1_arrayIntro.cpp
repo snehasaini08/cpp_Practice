6d0eec85007ab8895276a33a98fcdfb8aa15e6d1
Sneha <snehasaini143786@gmail.com>
2026-05-18 10:24:31 +0530

    commit changes

--- Lectures_codes.cpp/4_Array.cpp/1_arrayIntro.cpp
+++ Lectures_codes.cpp/4_Array.cpp/1_arrayIntro.cpp
@@ -9,8 +9,43 @@ int main()
     //accessing an arry
     cout << "Value at 15 index " << number[6] <<endl;
 
-    cout << "Value  at 20th index  " << number[30] << endl;
+    //cout << "Value  at 20th index  " << number[20] << endl;
            
-    cout << endl << "Everything is fine" << endl;
+   
+
+    //initialising an array 
+    int second[3] = {5,7,11};
+
+    //accessing an element
+    cout <<" Value at index 2" << second[2] << endl;
+
+    int third[15]= {2,7};
+    int n = 15;
+    cout << "Printing an array" << endl;
+    //print n array
+    for(int i=0;i<n;i++)
+    {
+        cout << third[i] << " ";
+    }
+
+
+    int fourth[10] = {0} ;
+    cout << "Printing an array" << endl;
+     //print n array
+    for(int i=0;i<n;i++)
+    {
+        cout << third[i] << " ";
+    }
+
+    //initialising all location with 1 (not possible with below ine)
+    int fifth[10] = {1};
+    n=10;
+    cout << "Printing the array" << endl;
+    //print the array
+    for(int i = 0;i<n;i++){
+        cout << fifth[i] << " ";
+    }
+
+     cout << endl << "Everything is fine" << endl;
     return 0;
 }
---
+++ Lectures_codes.cpp/4_Array.cpp/2_array_with_func.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+//HOLD
+void printarray(int arr[],int size){
+    cout << "Printing the array" <<endl;
+    //print an array
+    for(int i=0 ; i<size ; i++){
+        cout << arr[i] << " ";
+    }
+    cout << "printing done" << endl;
+}
+
+int main()
+{
+    int second[3] = {5,7,11};
+    cout <<" Value at index 2 " << second[2] << endl;
+
+    int third[15]= {2,7};
+    int n = 15;
+    printarray(third,15);
+
+
+    int fourth[10]= {0};
+    n = 10;
+    printarray(fourth,10);
+
+    int fifth[10] = {1};
+    n=10;
+    printarray(fifth,10);
+}
