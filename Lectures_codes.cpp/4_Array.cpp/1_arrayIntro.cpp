49812522d2a8ae29b67a926a39ba868106432e6c
Sneha <snehasaini143786@gmail.com>
2026-05-18 08:38:53 +0530

    commit changes

--- Lectures_codes.cpp/4_Array.cpp/1_arrayIntro.cpp
+++ Lectures_codes.cpp/4_Array.cpp/1_arrayIntro.cpp
@@ -9,7 +9,7 @@ int main()
     //accessing an arry
     cout << "Value at 15 index " << number[6] <<endl;
 
-    cout << "Value at 20th index  " << number[30] << endl;
+    cout << "Value  at 20th index  " << number[30] << endl;
            
     cout << endl << "Everything is fine" << endl;
     return 0;
