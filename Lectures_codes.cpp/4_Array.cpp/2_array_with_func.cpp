3f5902e96368b4291a856e2c397ff82fe944595c
Sneha <snehasaini143786@gmail.com>
2026-05-19 21:49:04 +0530

    commit changes

--- Lectures_codes.cpp/4_Array.cpp/2_array_with_func.cpp
+++ Lectures_codes.cpp/4_Array.cpp/2_array_with_func.cpp
@@ -19,6 +19,8 @@ int main()
     int third[15]= {2,7};
     int n = 15;
     printarray(third,15);
+    int thirdsize = sizeof(third)/sizeof(int);
+    cout << "Size of third is " << fifthsize << endl;
 
 
     int fourth[10]= {0};
@@ -28,4 +30,8 @@ int main()
     int fifth[10] = {1};
     n=10;
     printarray(fifth,10);
+
+    //to find the length of array 
+    int fifthsize = sizeof(fifth)/sizeof(int);
+    cout << "Size of fifth is " << fifthsize << endl;
 }
