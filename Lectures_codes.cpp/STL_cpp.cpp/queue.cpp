44ad5ae7b1aa2b6dedd520ee186ad5ac1ae8ced1
Sneha <snehasaini143786@gmail.com>
2026-09-25 12:06:04 +0530

    commi changes

--- Lectures_codes.cpp/STL_cpp.cpp/queue.cpp
+++ Lectures_codes.cpp/STL_cpp.cpp/queue.cpp
@@ -4,13 +4,50 @@ using namespace std;
 
 int main(){
 
+    queue<int> first;
+    queue<int> second;
 
+    first.push(10);
+    first.push(20);
 
+    second.push(100);
+    second.push(200);
 
+    first.swap(second);
 
+    cout<< first.front() << " " << first.back() << endl;
+    //creation
+//     queue<int> q;
 
+//     //insertion
+//     q.push(10);  //10
+//     q.push(20);  //10 20
+//     q.push(30);  // 10 20 30
+//     q.push(40);   // 10 20 30 40
 
+//     cout<< q.size() << endl;
 
+//     cout << "Front: " << q.front() << endl;
+//      cout << "Back: " << q.back() << endl;
     
+
+//     //cout << q.clear() << endl; //we cant clear a quee it doesnt have a clear function in it
+
+//     q.pop(); // 20 30 40
+//     cout << q.size() << endl;
+
+//     if(q.empty() == true){
+//         cout << " queue is empty " << endl;
+//     }else cout << " Queue is not empty " << endl;
+
+
+
+
+
+
+
+
+ return 0;
+
 }
 
